5243912eac6612da633fcedee1e806ebf22487ce
TitaRuiz <ggp2002es@gmail.com>
2023-10-18 15:47:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String nombre = "Manolo";
+        System.out.println(nombre.toLowerCase());
+        System.out.println(nombre.toUpperCase());
+        System.out.println(nombre);
+        System.out.println(nombre.isEmpty());
+        System.out.println(nombre.indexOf("n"));
+        System.out.println(nombre.indexOf("o"));
+        System.out.println(nombre.lastIndexOf("o"));
+
+
+    }
+}
